f890c2044ac2f27b8b82160f8c0cb6556850e6b0
Martin Ueding <dev@martin-ueding.de>
2012-02-12 12:19:11 +0100

    Add tests, makefile for running them

---
+++ python_prototype/copyright_updater_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright © 2012 Martin Ueding <dev@martin-ueding.de>
+
+import unittest
+
+class CopyrightUpdaterTest(unittest.TestCase):
+    def test_true(self):
+        assert True, "Control positive."

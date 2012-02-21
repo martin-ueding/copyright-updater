452a77104babe35a67e3d68da30396bd1f1e31d6
Martin Ueding <dev@martin-ueding.de>
2012-02-21 13:23:16 +0100

    Add setup.py

---
+++ python/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# Copyright (c) 2012 Martin Ueding <dev@martin-ueding.de>
+
+from distutils.core import setup
+
+setup(
+    author = "Martin Ueding",
+    author_email = "dev@martin-ueding.de",
+    description = "Updates copyright years.",
+    name = "copyrightupdate",
+    py_modules = ["copyrightupdate"],
+    scripts = ["copyright_updater"],
+    version = "0.1",
+)

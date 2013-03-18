84fde6dbbd0474e955845a9af259d915377d58fb
Martin Ueding <dev@martin-ueding.de>
2013-03-18 22:41:08 +0100

    More info in setup.py

--- python/setup.py
+++ python/setup.py
@@ -10,9 +10,19 @@ __docformat__ = "restructuredtext en"
 setup(
     author = "Martin Ueding",
     author_email = "dev@martin-ueding.de",
+    classifiers = [
+        "Environment :: Console",
+        "Environment :: Plugins",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Programming Language :: Python",
+        "Topic :: Text Editors",
+
+    ],
     description = "Updates copyright years.",
     name = "copyrightupdate",
     py_modules = ["copyrightupdate"],
     scripts = ["copyright-updater"],
+    url = "http://martin-ueding.de/projects/copyright-updater/",
     version = "1.1.1",
 )
